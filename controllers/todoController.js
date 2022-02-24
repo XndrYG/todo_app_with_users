6e7d1b8cd116741898b7482df00329b6a07b2de8
XndrYG <xygarcia6406@gmail.com>
2022-02-24 08:34:51 -0800

    first error

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -36,12 +36,18 @@ module.exports.addNewItem = async function(req, res){
     res.redirect('/');
 };
 
-
 module.exports.viewEditItem = async function(req, res) {
-    const todo = await Todo.findByPk(req.params.id);
-    res.render('todos/editItem', {item: todo})
+    const todo = await Todo.findOne({
+        where: {
+            id: req.params.id,
+            user_id: req.user.id
+        }});
+    if (!todo) { //if we cant find it
+        res.redirect('/');
+    } else {
+        res.render('todos/editItem', {item: todo})
+    }
 };
-
 
 module.exports.saveEditItem = async function(req, res) {
     await Todo.update({ description: req.body.description}, {
@@ -52,11 +58,11 @@ module.exports.saveEditItem = async function(req, res) {
     res.redirect('/');
 };
 
-
 module.exports.deleteItem = async function(req, res) {
     await Todo.destroy({
         where: {
-            id: req.params.id
+            id: req.params.id,
+            user_id: req.params.id
         }
     })
     res.redirect('/');
